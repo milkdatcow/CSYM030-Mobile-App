9e955cf0854c3d01327097d0c8a990da6b6c137d
Dyl <bowyer008@gmail.com>
2025-03-19 15:47:03 +0000

    Asynchonus Storage
    Needs fixing

--- project/app/screens/Login.js
+++ project/app/screens/Login.js
@@ -1,10 +1,10 @@
-import { View, Text, TextInput, TouchableOpacity, StyleSheet, Image, Alert, AsyncStorage } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, Image, Alert } from 'react-native';
 import React from 'react'
 import styles from "../style"
 import Icon from 'react-native-vector-icons/Ionicons';
-import { useState } from 'react';
-
-let STORAGE_KEY = '@user_input'
+import { useState, useEffect } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Key } from 'react-native-feather';
 
 
 const Login = ({navigation}) => {
@@ -14,7 +14,13 @@ const Login = ({navigation}) => {
       {user:'John',pass:'password'},
       {user:'Charlie',pass:'password1'}
     ]
+    const test = {user:'John',pass:'password'}
   
+    useEffect(() => {
+      storeData(test);
+      testRetrieval();
+    }, []);
+
     function loginUser(){
       for(var i=0;i<users.length;i++){
         console.log(i+ " "+users[i].user);
@@ -25,50 +31,46 @@ const Login = ({navigation}) => {
         break;
        }else{
         Alert.alert("Login Failed...");
+        break;
        }
       }
       
     }
 
-    const[input,setInput] = useState("");
-
-    const saveData = async () => {
+    const storeData = async (value) => {
       try {
-        await AsyncStorage.setItem(STORAGE_KEY, age);
-        alert('data successfully saved')
+        const jsonValue = JSON.stringify(value);
+        await AsyncStorage.setItem('key', jsonValue);
       } catch (e) {
-        alert('error failed saving')
+        alert("error saving data")
       }
-    };
-
-    const dataRetrieve = async () => {
-      try {
-        const value = await AsyncStorage.getItem(STORAGE_KEY);
-        if (value !== null) {
-          setInput(value);
-        }
-      } catch (e) {
-        alert('error retrieving data')
-      }
-    };
-
-    const onChangeText = value => setInput(value);
-
-    const onSubmitEditing = () =>{
-      if(!input) return;
-
-      saveData(input);
-      setInput("");
     }
+
+    const readItems = async (key) => {
+      try {
+        const jsonValue = await AsyncStorage.getItem(key);
+        return jsonValue != null ? JSON.parse(jsonValue) : [];
+      } catch (error) {
+        console.log('Error reading data', error);
+      }
+      };
+
+      const clearStorage = async () => {
+        try {
+          await AsyncStorage.clear();
+        } catch (e) {
+          alert('Failed to clear the async storage.');
+        }
+      };
+
+    
 
 
   return (
     <View style={styles.container}>
     <View><Text style={styles.h1}>Login</Text></View>
-    <View><TextInput placeholder='Enter input' style={styles.input} value={input} onChangeText={onChangeText} onSubmitEditing={onSubmitEditing}></TextInput>
-    <Text>Your input is {input}</Text>
+    <View><TextInput placeholder='Enter input' style={styles.input} value={userName} onChangeText={text=>setUserName(text)}></TextInput>
     </View>
-    
     
     <View><TextInput placeholder='Enter password' style={styles.input} secureTextEntry={true} value={password} onChangeText={text=>setPassword(text)}></TextInput></View>
     <TouchableOpacity style={styles.button} onPress={()=>loginUser(userName,password)}><Text>Sign In</Text></TouchableOpacity>
