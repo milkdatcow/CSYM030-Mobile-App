e61808095f03f05b57035a56becc84f208313e1c
Dyl <bowyer008@gmail.com>
2025-03-26 15:24:19 +0000

    Update Calculator.js

--- project/app/screens/Calculator.js
+++ project/app/screens/Calculator.js
@@ -32,25 +32,27 @@ export default function App() {
         <TouchableOpacity style={styles.cbutton} onPress={() => handlePress("7")}><Text>7</Text></TouchableOpacity>
         <TouchableOpacity style={styles.cbutton} onPress={() => handlePress("8")}><Text>8</Text></TouchableOpacity>
         <TouchableOpacity style={styles.cbutton} onPress={() => handlePress("9")}><Text>9</Text></TouchableOpacity>
-        <TouchableOpacity style={styles.opbutton} onPress={() => handlePress("-")}><Text>-</Text></TouchableOpacity>
+        <TouchableOpacity style={styles.opbutton} onPress={clearInput}>
+          <Text>Clear</Text>
+        </TouchableOpacity>
       </View>
       <View style={styles.row}>
         <TouchableOpacity style={styles.cbutton} onPress={() => handlePress("4")}><Text>4</Text></TouchableOpacity>
         <TouchableOpacity style={styles.cbutton} onPress={() => handlePress("5")}><Text>5</Text></TouchableOpacity>
         <TouchableOpacity style={styles.cbutton} onPress={() => handlePress("6")}><Text>6</Text></TouchableOpacity>
-        <TouchableOpacity style={styles.opbutton} onPress={() => handlePress("+")}><Text>+</Text></TouchableOpacity>
+        <TouchableOpacity style={styles.opbutton} onPress={() => handlePress("*")}><Text>*</Text></TouchableOpacity>
       </View>
       <View style={styles.row}>
         <TouchableOpacity style={styles.cbutton} onPress={() => handlePress("1")}><Text>1</Text></TouchableOpacity>
         <TouchableOpacity style={styles.cbutton} onPress={() => handlePress("2")}><Text>2</Text></TouchableOpacity>
         <TouchableOpacity style={styles.cbutton} onPress={() => handlePress("3")}><Text>3</Text></TouchableOpacity>
-        <TouchableOpacity style={styles.opbutton} onPress={calculateResult}><Text>=</Text></TouchableOpacity>
+        <TouchableOpacity style={styles.opbutton} onPress={() => handlePress("/")}><Text>/</Text></TouchableOpacity>
       </View>
       <View style={styles.row}>
         <TouchableOpacity style={styles.cbutton} onPress={() => handlePress("0")}><Text>0</Text></TouchableOpacity>
-        <TouchableOpacity style={styles.opbutton} onPress={clearInput}>
-          <Text>Clear</Text>
-        </TouchableOpacity>
+        <TouchableOpacity style={styles.opbutton} onPress={() => handlePress("+")}><Text>+</Text></TouchableOpacity>
+        <TouchableOpacity style={styles.opbutton} onPress={() => handlePress("-")}><Text>-</Text></TouchableOpacity>
+        <TouchableOpacity style={styles.opbutton} onPress={calculateResult}><Text>=</Text></TouchableOpacity>
       </View>
     </View>
   );
